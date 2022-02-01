f76faed79e82fb158850d3b4ec7efed1498bfaec
pallomagualter <pallomagualter@gmail.com>
2022-02-01 13:37:42 -0300

    change Mode

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ function changeMode() {
 }
 
 function changeClasses() {
-    button.classList.toggle('dark-mode');
-    h1.classList.toggle('dark-mode');
-    body.classList.toggle('dark-mode');
-    footer.classList.toggle('dark-mode');
+    button.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
 }
 
 function changeText() {
-    //if(button.classList.contains('dark-mode'))
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode"
+    if(body.classList.contains(darkModeClass)){
+        // innerHTML é html dentro do botão
+        button.innerHTML = lightMode;
+        h1.innerHTML = darkMode + " ON";
+        return;
+    }
+
+    button.innerHTML = darkMode;
+    h1.innerHTML = lightMode + " ON";
 }
 
+const darkModeClass = 'dark-mode';
 const button = document.getElementById('mode-selector');
 const h1 = document.getElementById('page-title');
 const body = document.getElementsByTagName('body')[0];
