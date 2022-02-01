be881575739bc466ececc188cfb913a867056337
pallomagualter <pallomagualter@gmail.com>
2022-02-01 13:30:01 -0300

    dark-mode

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function changeMode() {
-    console.log('Cliquei!');
+    changeClasses();
+    changeText();
+}
+
+function changeClasses() {
+    button.classList.toggle('dark-mode');
+    h1.classList.toggle('dark-mode');
+    body.classList.toggle('dark-mode');
+    footer.classList.toggle('dark-mode');
+}
+
+function changeText() {
+    //if(button.classList.contains('dark-mode'))
 }
 
 const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
 
 button.addEventListener('click', changeMode);
